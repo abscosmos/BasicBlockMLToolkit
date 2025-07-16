7842bfc1988b7e6329c030548d2aed5b1b30ab96
abscosmos <joshua@dinoslice.com>
2025-07-16 11:24:10 -0500

    Create function to get arguments from argc & argv

--- logger/src/lib.rs
+++ logger/src/lib.rs
@@ -1,7 +1,9 @@
+use std::borrow::Cow;
 use std::io::Write as _;
 use std::ffi::{c_char, c_int, c_void, CStr};
 use std::fs::File;
 use std::ptr::NonNull;
+use std::slice;
 use dynamorio_sys::{bool_, client_id_t, dr_emit_flags_t, dr_lookup_module, dr_module_preferred_name, dr_register_bb_event, dr_register_exit_event, dr_set_client_name, instr_get_app_pc, instrlist_first_app, instrlist_t};
 use parking_lot::Mutex;
 
@@ -13,6 +15,20 @@ struct Logger {
 }
 
 static LOGGER: Mutex<Option<Logger>> = Mutex::new(None);
+
+/// # Safety
+/// - `argc` and `argv` must be from main call
+/// - `argv` should point to `argc` number of valid null terminated c strings
+/// - the array pointed to by `argv` & the c strings should be
+/// valid for the entire duration of the program (`'static`)
+unsafe fn collect_args<'a>(argc: c_int, argv: *const *const c_char) -> impl Iterator<Item=Cow<'static, str>> {
+    let args = unsafe { slice::from_raw_parts(argv, argc as _) };
+
+    args.into_iter()
+        .map(|&ptr|
+            unsafe { CStr::from_ptr(ptr) }.to_string_lossy()
+        )
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn dr_client_main(
