020a4768a7cf72c496111dfefddd89888e8d0e51
abscosmos <joshua@dinoslice.com>
2025-07-17 10:18:28 -0500

    Create BlockCountStats from iterator

--- core/src/trace.rs
+++ core/src/trace.rs
@@ -23,24 +23,14 @@ impl TraceData {
                 .keys()
                 .map(|k| &k.application);
 
-            let targeted = BlockCountStats {
-                num_blocks: iter.clone().count(),
-                num_unique_blocks: unique_ct(iter.clone()),
-                num_unique_symbolized: unique_ct(iter.map(BasicBlock::symbolize)),
-            };
-
-            (Some(targeted), Some(unique_ct(apps)))
+            (Some(BlockCountStats::from_iter(iter)), Some(unique_ct(apps)))
         } else {
             (None, None)
         };
 
         TraceSummary {
             application: self.targeted.clone(),
-            counts: BlockCountStats {
-                num_blocks: self.blocks.len(),
-                num_unique_blocks: unique_ct(self.blocks.values()),
-                num_unique_symbolized: unique_ct(self.blocks.values().map(BasicBlock::symbolize)),
-            },
+            counts: BlockCountStats::from_iter(self.blocks.values()),
             targeted,
             unique_apps,
         }
@@ -60,6 +50,18 @@ pub struct BlockCountStats {
     num_blocks: usize,
     num_unique_blocks: usize,
     num_unique_symbolized: usize,
+}
+
+impl BlockCountStats {
+    pub fn from_iter<'a>(iter: impl IntoIterator<Item=&'a BasicBlock, IntoIter: Clone>) -> Self {
+        let iter = iter.into_iter();
+
+        Self {
+            num_blocks: iter.clone().count(),
+            num_unique_blocks: unique_ct(iter.clone()),
+            num_unique_symbolized: unique_ct(iter.map(BasicBlock::symbolize)),
+        }
+    }
 }
 
 fn unique_ct<T: Hash + Eq>(iter: impl IntoIterator<Item=T>) -> usize {
