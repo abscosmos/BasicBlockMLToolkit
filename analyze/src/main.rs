ce73c47565a0cbc53e0931d51d04ac307212d413
abscosmos <joshua@dinoslice.com>
2025-07-17 09:17:03 -0500

    Use anyhow for errors

--- analyze/src/main.rs
+++ analyze/src/main.rs
@@ -1,19 +1,27 @@
 use std::{fs, io};
+use hashbrown::HashSet;
 use std::ffi::OsStr;
 use std::path::Path;
+use anyhow::Context;
 use logger_core::TraceData;
 
 fn main() {
-    println!("Hello, world!");
+    let traces = load_traces(&["ls", "cd"]).unwrap();
+
+    let trace_apps = traces.iter()
+        .map(|t| t.targeted.name.as_ref())
+        .collect::<HashSet<_>>();
+
+    println!("Loaded traces for {trace_apps:?}");
 }
 
-fn load_traces(applications: &[impl AsRef<str>]) -> io::Result<Vec<TraceData>> {
+fn load_traces(applications: &[impl AsRef<str>]) -> anyhow::Result<Vec<TraceData>> {
     let search_dir = Path::new("./traces");
 
     let mut traces = Vec::new();
 
-    for entry in fs::read_dir(search_dir)? {
-        let path = entry?.path();
+    for entry in fs::read_dir(search_dir).context("failed to read \"./traces\"")? {
+        let path = entry.context("failed to get dir entry")?.path();
 
         if path.is_file()
             && path.extension() == Some(OsStr::new("trace"))
@@ -25,10 +33,10 @@ fn load_traces(applications: &[impl AsRef<str>]) -> io::Result<Vec<TraceData>> {
                     )
                 )
         {
-            let bytes = fs::read(path)?;
+            let bytes = fs::read(path).context("failed to read bytes")?;
 
             let trace_data = postcard::from_bytes(&bytes)
-                .expect("TODO: error handling; binary file wasn't valid");
+                .context("failed to deserialize trace data")?;
 
             traces.push(trace_data);
         }
