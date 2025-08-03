26bdec1b859d9a76f71baa308a039a1ebfc9daa0
abscosmos <joshua@dinoslice.com>
2025-08-03 14:35:38 -0500

    Test tokenizer

--- ml/main.py
+++ ml/main.py
@@ -1,8 +1,19 @@
 from bb_toolkit import TraceData
+from ml.tokenizer import BasicBlockTokenizer
 
 def main():
     trace = TraceData.from_binary_file("../traces/ls_trace-688eab66.trace")
-    print(len(trace.blocks))
+    tokenizer = BasicBlockTokenizer()
+
+    sequence = tokenizer.process_trace(trace)
+
+    print(sequence[:20])
+
+    for i in range(6,10):
+        block = trace.blocks[trace.order[i]]
+
+        tk = sequence[i]
+        print(f"{tk}:\n{tokenizer.get_block(tk)}\n=>\n{block}\n")
 
 
 if __name__ == "__main__":
