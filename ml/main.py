9d22fe494a64c87844c7f8f1d2aeca3e0d52f056
abscosmos <joshua@dinoslice.com>
2025-08-03 21:08:37 -0500

    Create test ml pipeline

--- ml/main.py
+++ ml/main.py
@@ -1,8 +1,16 @@
 import os
 from pathlib import Path
+from typing import Literal
 
-from bb_toolkit import TraceData
-from ml.tokenizer import BasicBlockTokenizer
+import torch
+
+from bb_toolkit import TraceData, BasicBlockTokenizer
+from torch.optim import AdamW
+
+from ml.dataset import analyze_sequence_stats, create_training_data
+from ml.model import create_model
+from ml.train import train_epoch
+
 
 def load_all_traces(traces_dir: os.PathLike) -> list[TraceData]:
     files = sorted(Path(traces_dir).glob("*.trace"), key=lambda f: f.stat().st_ctime)
@@ -10,18 +18,50 @@ def load_all_traces(traces_dir: os.PathLike) -> list[TraceData]:
     return [TraceData.from_binary_file(file) for file in files]
 
 def main():
-    trace = TraceData.from_binary_file("../traces/ls_trace-688eab66.trace")
+    model_save_path = "./best_model.pt"
+
+    # 1. load traces
+    trace_path = "../bulk_collect/traces"
+    all_traces = load_all_traces(trace_path)
+
+    print(f"Loaded {len(all_traces)} from {trace_path}.")
+
+    # 2. tokenize
     tokenizer = BasicBlockTokenizer()
+    sequences: list[list[int]] = [tokenizer.process_trace(trace) for trace in all_traces]
 
-    sequence = tokenizer.process_trace(trace)
+    # 3. stats
+    print(analyze_sequence_stats(sequences))
 
-    print(sequence[:20])
+    # 4. partition data
+    sequence_length = 16
+    train_data, val_data, test_data = create_training_data(
+        tokenized_sequences=sequences,
+        sequence_length=sequence_length,
+        validation_size=0.15,
+        test_size=0.15,
+        batch_size=8,
+    )
 
-    for i in range(6,10):
-        block = trace.blocks[trace.order[i]]
+    # 5. training
+    num_epochs = 2
 
-        tk = sequence[i]
-        print(f"{tk}:\n{tokenizer.get_block(tk)}\n=>\n{block}\n")
+    vocab_size = len(tokenizer)
+    device: Literal["cpu", "cuda"] = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model = create_model(vocab_size, context_length=sequence_length).to(device)
+    optimizer = AdamW(model.parameters(), lr=1e-4)
+    best_val_loss = float("inf")
+
+    for epoch in range(num_epochs):
+        train_loss, val_loss = train_epoch(model, optimizer, device, train_data, val_data)
+
+        print(f"epoch {epoch+1}: train loss {train_loss:.4f}, val loss {val_loss:.4f}")
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save(model.state_dict(), model_save_path)
+            print("saved new best model")
 
 
 if __name__ == "__main__":
