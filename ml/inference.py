ec9eb49c98aae81b151cd42c37463da2e0e1852e
abscosmos <joshua@dinoslice.com>
2025-08-06 20:24:27 -0500

    Accept tokens or blocks in make_inference

--- ml/inference.py
+++ ml/inference.py
@@ -6,16 +6,21 @@ def make_inference(
     model: BasicBlockPredictor,
     tokenizer: BasicBlockTokenizer,
 
-    input: list[SymbolizedBasicBlock],
+    input_seq: list[SymbolizedBasicBlock] | list[int],
     context_len: int,
 
     top_k: int,
 ) -> list[tuple[SymbolizedBasicBlock, float]]:
+    if len(input_seq) == 0 or isinstance(input_seq[0], int):
+        input_tokens: list[int] = input_seq
+    else:
+        input_tokens: list[int] = [tokenizer.get_token(block) for block in input_seq]
+
     # tokenize first context_len blocks
-    sequence = (
+    sequence: list[int] = (
         # add padding tokens if necessary
-        [BasicBlockTokenizer.PADDING_TOKEN] * max(0, context_len - len(input))
-        + [tokenizer.get_token(block) for block in input[-context_len:]]
+        [BasicBlockTokenizer.PADDING_TOKEN] * max(0, context_len - len(input_seq))
+        + input_tokens[-context_len:]
     )
 
     input_tensor = torch.tensor(sequence, dtype=torch.long).to(model.device)
