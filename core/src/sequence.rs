3c79fd8c52bdc28d3d9f7e5fc561cd025b9ae514
abscosmos <joshua@dinoslice.com>
2025-08-14 17:37:45 -0500

    Replace OrderMap with Box<[]> in BasicBlockSequence
    Duplicate entries will most likely exist, which isn't won't work with OrderMap.

--- core/src/sequence.rs
+++ core/src/sequence.rs
@@ -1,6 +1,5 @@
-use ordermap::OrderMap;
 use crate::{BasicBlock, BasicBlockLocation as BlockLoc};
 
 pub struct BasicBlockSequence {
-    pub blocks: OrderMap<BlockLoc, BasicBlock>,
+    pub blocks: Box<[(BlockLoc, BasicBlock)]>,
 }
