cb562674e277ab03181298a5207e655e00f397dc
abscosmos <joshua@dinoslice.com>
2025-08-14 17:44:23 -0500

    Create function to get iterator over blocks in basic block sequence

--- core/src/sequence.rs
+++ core/src/sequence.rs
@@ -1,3 +1,11 @@
 use crate::{BasicBlock, BasicBlockLocation as BlockLoc};
 
 pub struct BasicBlockSequence(pub Box<[(BlockLoc, BasicBlock)]>);
+
+impl BasicBlockSequence {
+    pub fn blocks(&self) -> impl Iterator<Item=&BasicBlock> {
+        self.0
+            .iter()
+            .map(|(_, block)| block)
+    }
+}
